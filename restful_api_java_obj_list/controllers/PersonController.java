b1e45c03f5e85d5e3aafb34e993903ab725d21a4
IanSilvaCertacon <ian.silva@certacon.com.br>
2024-11-19 15:51:53 -0300

    flyway migration

--- restful_api_java_obj_list/controllers/PersonController.java
+++ restful_api_java_obj_list/controllers/PersonController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("api/person/v1")
 public class PersonController {
 
     @Autowired //junto com o @Service ele linka um com o outro igual o comentario abaixo
